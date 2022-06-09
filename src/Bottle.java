80b7fc3f31b8764bada1342205e3c4547a915932
buc-htl <55023682+buc-htl@users.noreply.github.com>
2022-06-09 11:57:57 +0200

    Update Bottle.java
    Dokumentation verbessert

--- src/Bottle.java
+++ src/Bottle.java
@@ -1,12 +1,13 @@
 /**
-In Java kann eine Klasse Methoden und Konstruktoren haben, die den gleichen Namen besitzen. Dies nett man "Überladen".
+In Java kann eine Klasse Methoden und Konstruktoren haben, die den gleichen Namen besitzen. Dies nennt man "Überladen".
 
 Damit das Überladen möglich ist, muss wenigstens eine der folgenden Vorraussetzungen erfüllt sein:
 
 - Der Datentyp mindestens eines Übergabeparameters ist anders als in den übrigen gleichnamen Methoden.
 - Die Anzahl der Übergabeparameter ist unterschiedlich.
 
-Das erlaubt den Compiler eindeutig zu bestimmen welche Methode denn schlussendlich aufgerufen werden soll. Der Rückgabewert muss bei allen überladenen Methoden immer gleich sein.
+Das erlaubt den Compiler eindeutig zu bestimmen welche Methode denn schlussendlich aufgerufen werden soll. 
+Der Rückgabewert muss bei allen überladenen Methoden immer gleich sein.
 */
 
 public class Bottle {
@@ -41,11 +42,12 @@ public class Bottle {
     }
 
   /**
-    Auch Methoden kann man überladen. 
+    Auch Methoden kann man überladen.
+    Damit können auch in Java "Default-Werte" für einzelne Parameter umgesetzt werden.
   **/
     public void reset() {
         System.out.println("resetBottle ohne Parameter.");
-        reset(0,0);
+        reset(0,0); //setze Volumen und Level auf 0 (Default Werte in unserem Beispiel)
     }
 
     public void reset(double volume){
